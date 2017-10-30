73d15da08218aaf25ff61c5116d3888c181e3cc7
onlyforld <mlxgzzz@outlook.com>
2017-10-30 14:51:32 +0800

    tiny add ts

---
+++ number.ts
@@ -0,0 +1,24 @@
+// class User{
+//     name:string;
+//     id:number;
+//     constructor(name:string,id:number){
+//         this.name = name
+//         this.id = id
+//     }
+//     validateName(newName:string){
+//         if(!newName){
+//             throw Error("name is invalid");
+//         }
+//     }
+//     changeName(newName:string){
+//         this.name = newName
+//     }
+// }
+// let user = new User("tom",1)
+// if(user.validateName("tasaid")){
+//     user.changeName("tasaid");
+// }
+function add(a:number,b:number):number{
+        return (a + b);
+}
+console.log(add(2,5));
