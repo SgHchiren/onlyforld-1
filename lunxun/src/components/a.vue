054874c6dca10061d440a5cacff2d3e288e9f5b0
onlyforld <mlxgzzz@outlook.com>
2017-11-17 12:00:53 +0800

    pull down refresh

--- lunxun/src/components/a.vue
+++ lunxun/src/components/a.vue
@@ -32,7 +32,7 @@ import { Toast,Button,Indicator,Loadmore } from 'mint-ui';
     data(){
       return{
         arr:[1,2,3],
-        list:[1,2, 3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+        list:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
         limit:10,
         page:0
       }
@@ -48,17 +48,22 @@ import { Toast,Button,Indicator,Loadmore } from 'mint-ui';
         },5000)
       },
       loadTop(){
-        let {page,limit} = this;
+        let {limit} = this;
         this.$http.get("../../src/data/b.json").then(res=>{
-          let data = res.body.come.slice(page * limit,limit * (page + 1));
+          let data = res.body.come.slice(this.page * limit,limit * (this.page + 1));
           let len = data.length;
           for(let i = 0; i < len; i ++){
             this.list.unshift(data[i].a);
             console.log("加载完成了")
           }
           console.log("我加载了更多数据");
+          if(this.page <= 9){
+            this.page++;
+          }else{
+            this.page = 10;
+          }
           this.$refs.loadmore.onTopLoaded();
-          console.log(page);
+          console.log(this.page);
         }).catch(err=>{
           console.log(err)
         })
